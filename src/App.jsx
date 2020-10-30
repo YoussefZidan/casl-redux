9516dbf2ecb41b905543c18af629ced761e1b463
Youssef Zidan <youssefzidan2200@gmail.com>
2020-10-30 21:38:53 +0200

    reorganizing the code

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import CAN from "./casl/can";
 import { login, logout } from "./redux/auth/authActions";
+import CAN from "./casl/can";
 
 export default () => {
   const dispatch = useDispatch();
--- src/casl/can.js
+++ src/casl/can.js
@@ -3,6 +3,10 @@ import { store } from "../redux/storeConfig/store";
 
 const ability = new Ability();
 
+export default (action, subject) => {
+  return ability.can(action, subject);
+};
+
 store.subscribe(() => {
   let auth = store.getState().auth;
   ability.update(defineRulesFor(auth));
@@ -23,25 +27,3 @@ const defineRulesFor = (auth) => {
 
   return rules;
 };
-
-export default (action, subject) => {
-  return ability.can(action, subject);
-};
-
-// // #2
-// if (permissions) {
-//   permissions.forEach((p) => {
-//     if (p === 3) can("add", "users");
-//     if (p === 7) can("delete", "users");
-//   });
-// }
-
-// // #3
-// if (permissions) {
-//   permissions.forEach((p) => {
-//     if (p === "Admin") {
-//       can("add", "users");
-//       can("delete", "users");
-//     }
-//   });
-// }
